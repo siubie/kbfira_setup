de7ff9ff3e77bff91dd46a0751effe91fcfaaa11
Putra Prima Arhandi <putraprima@gmail.com>
2024-05-20 21:13:01 +0900

    change db

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,6 @@
-CREATE DATABASE `kbv2-fira` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'kbv2user'@'localhost' IDENTIFIED WITH mysql_native_password BY 'kbv2userpass';
-CREATE USER 'kbv2user'@'%' IDENTIFIED WITH mysql_native_password BY 'kbv2userpass';
-GRANT ALL PRIVILEGES ON `kbv2-fira`.* TO `kbv2user`@`localhost`;
-GRANT ALL PRIVILEGES ON `kbv2-fira`.* TO `kbv2user`@`%`;
+CREATE DATABASE `kb_v2_fira` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+CREATE USER 'superuser'@'localhost' IDENTIFIED WITH mysql_native_password BY '@superUser!123';
+CREATE USER 'superuser'@'%' IDENTIFIED WITH mysql_native_password BY '@superUser!123';
+GRANT ALL PRIVILEGES ON `kb_v2_fira`.* TO `kbv2user`@`localhost`;
+GRANT ALL PRIVILEGES ON `kb_v2_fira`.* TO `kbv2user`@`%`;
 FLUSH PRIVILEGES;
