8f41e2ee217b35b4e185ab925ef06a8399f1145b
Putra Prima <putraprima@gmail.com>
2024-05-22 15:11:15 +0900

    Update MySQL version and set password for superuser

--- mysql/docker-entrypoint-initdb.d/init.sql
+++ mysql/docker-entrypoint-initdb.d/init.sql
@@ -1,6 +1,51 @@
 CREATE DATABASE `kb_v2_fira` CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
-CREATE USER 'superuser'@'localhost' IDENTIFIED WITH mysql_native_password BY '@superUser!123';
-CREATE USER 'superuser'@'%' IDENTIFIED WITH mysql_native_password BY '@superUser!123';
+CREATE USER 'superuser'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password';
+CREATE USER 'superuser'@'%' IDENTIFIED WITH mysql_native_password BY 'password';
 GRANT ALL PRIVILEGES ON `kb_v2_fira`.* TO `superuser`@`localhost`;
 GRANT ALL PRIVILEGES ON `kb_v2_fira`.* TO `superuser`@`%`;
 FLUSH PRIVILEGES;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
